334bd37d928eeef9c422122a9f6624d71209199c
lemossc <lemossc@oregonstate.edu>
2021-03-03 11:33:08 -0800

    Added code to pass test_buzz()

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,6 +3,8 @@ def fizz_buzz():
     for number in range(1, 100, 1):
         if number % 3 == 0:
             result[number - 1] = "Fizz"
+        elif number % 5 == 0:
+            result[number - 1] = "Buzz"
 
     print(result)
     return result
