9069ba0eff0625ea32b80455633f26d74f8a0f3a
lemossc <lemossc@oregonstate.edu>
2021-03-03 11:08:23 -0800

    Modified test_fizz() to test all multiplies of 3

--- fizz_buzz_test.py
+++ fizz_buzz_test.py
@@ -4,10 +4,12 @@ import fizz_buzz
 
 class TestFizzBuzz(unittest.TestCase):
     def test_fizz(self):
-        result = []
-        result.append(fizz_buzz.fizz_buzz(3))
-        for output in result:
-            self.assertEqual(output, "Fizz")
+        result = fizz_buzz.fizz_buzz()
+        i = 0
+        for element in result:
+            if i % 3 == 0:s
+                self.assertEqual(element, "Fizz")
+            i += 1
 
 
 if __name__ == "__main__":
