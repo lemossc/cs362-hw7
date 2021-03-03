c2f6b689695ca79c64d10670b3b8b825d18f6e25
lemossc <lemossc@oregonstate.edu>
2021-03-03 11:45:54 -0800

    Fixed tests to start from i = 1

--- fizz_buzz_test.py
+++ fizz_buzz_test.py
@@ -5,23 +5,23 @@ import fizz_buzz
 class TestFizzBuzz(unittest.TestCase):
     def test_fizz(self):
         result = fizz_buzz.fizz_buzz()
-        i = 0
+        i = 1
         for element in result:
-            if i % 3 == 0:
+            if i % 3 == 0 and i % 5 != 0:
                 self.assertEqual(element, "Fizz")
             i += 1
 
     def test_buzz(self):
         result = fizz_buzz.fizz_buzz()
-        i = 0
+        i = 1
         for element in result:
-            if i % 5 == 0:
+            if i % 5 == 0 and i % 3 != 0:
                 self.assertEqual(element, "Buzz")
             i += 1
 
     def test_fizz_buzz(self):
         result = fizz_buzz.fizz_buzz()
-        i = 0
+        i = 1
         for element in result:
             if i % 3 == 0 and i % 5 == 0:
                 self.assertEqual(element, "FizzBuzz")
