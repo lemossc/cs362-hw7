24b1058d485cee6af5104824ad3d0ff24007bbb6
lemossc <lemossc@oregonstate.edu>
2021-03-03 11:44:29 -0800

    Added code to pass test_fizz_buzz()

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,7 +1,9 @@
 def fizz_buzz():
     result = [None] * 100
-    for number in range(1, 100, 1):
-        if number % 3 == 0:
+    for number in range(1, 100 + 1, 1):
+        if number % 3 == 0 and number % 5 == 0:
+            result[number - 1] = "FizzBuzz"
+        elif number % 3 == 0:
             result[number - 1] = "Fizz"
         elif number % 5 == 0:
             result[number - 1] = "Buzz"
