eee710cbd25ce020765e823fcd40bf7ecbec5644
lemossc <lemossc@oregonstate.edu>
2021-03-03 17:23:15 -0800

    Added code to validate input and pass test_invalid_input()

--- leap_year.py
+++ leap_year.py
@@ -1,4 +1,11 @@
+FIRST_LEAP_YEAR = 1582  # First year leap year rules were introduced by the Gregorian Calendar
+
+
 def is_leap_year(year):
+    # Validate input
+    if not isinstance(year, int) or year < FIRST_LEAP_YEAR:
+        return None
+    # Determine whether year is a leap year -- assume common
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
