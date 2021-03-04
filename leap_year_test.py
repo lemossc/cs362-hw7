e46ed1b1651c228155d595aca42dd98771826874
lemossc <lemossc@oregonstate.edu>
2021-03-03 17:14:53 -0800

    Implemented test_common_years() to test non-leap years

--- leap_year_test.py
+++ leap_year_test.py
@@ -9,6 +9,12 @@ class TestLeapYear(unittest.TestCase):
         result = leap_year.is_leap_year(2004)
         self.assertEqual(result, True)
 
+    def test_common_years(self):
+        result = leap_year.is_leap_year(2001)
+        self.assertEqual(result, False)
+        result = leap_year.is_leap_year(2100)
+        self.assertEqual(result, False)
+
 
 if __name__ == "__main__":
     unittest.main()
